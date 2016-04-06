2fe53097ba78279e34b651cacf6261fe50db3fd5
mrtycjan <mrtycjan@protonmail.ch>
2016-04-06 15:11:24 +0200

    Space Ambient

--- Collision.h
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-
-#include "BitmaskManager.h"
-class Collision
-{
-public:
-
-    static bool pixelPerfect(const sf::Sprite& Object1,const sf::Sprite& Object2, sf::Uint8 AlphaLimit = 0);
-};
